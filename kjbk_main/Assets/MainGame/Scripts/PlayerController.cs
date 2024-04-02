d40a3cfb5cdeb19e96ff905a368d28f236b1143a
LAPTOP-B3ETC2GO\matsu <nicokarma2050@gmail.com>
2024-04-02 22:58:57 +0900

    2024/04/02 石崎 移動スクリプトの修正

--- kjbk_main/Assets/MainGame/Scripts/PlayerController.cs
+++ kjbk_main/Assets/MainGame/Scripts/PlayerController.cs
@@ -23,25 +23,34 @@ public class PlayerController : MonoBehaviour
 	}
     void Update()
     {
-        // 左シフト(ダッシュ)が押されているか判定
-        if (Input.GetKey(KeyCode.LeftShift))
+        Rigidbody rb = this.transform.GetComponent<Rigidbody>();
+
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
         {
-            CurrentSpeed = RunSpeed;
+            rb.velocity = Vector3.zero;
         }
         else
         {
-            CurrentSpeed = Speed;
+            // 左シフト(ダッシュ)が押されているか判定
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                CurrentSpeed = RunSpeed;
+            }
+            else
+            {
+                CurrentSpeed = Speed;
+            }
+
+            //垂直方向と水平方向の入力を取得
+            float Xvalue = Input.GetAxis("Horizontal") * CurrentSpeed * Time.deltaTime;
+            float Yvalue = Input.GetAxis("Vertical") * CurrentSpeed * Time.deltaTime;
+
+            //位置を移動
+            Vector3 MoveDir = new Vector3(Xvalue, 0, Yvalue);
+            rb.velocity += MoveDir;
+
+            //進行方向を向く
+            transform.forward = Vector3.Slerp(transform.forward, MoveDir, Time.deltaTime * RotateSpeed);
         }
-
-        //垂直方向と水平方向の入力を取得
-        float Xvalue = Input.GetAxis("Horizontal") * CurrentSpeed * Time.deltaTime;
-        float Yvalue = Input.GetAxis("Vertical") * CurrentSpeed * Time.deltaTime;
-
-        //位置を移動
-        Vector3 MoveDir = new Vector3(Xvalue, 0, Yvalue);
-        transform.position += MoveDir;
-
-        //進行方向を向く
-        transform.forward = Vector3.Slerp(transform.forward, MoveDir, Time.deltaTime * RotateSpeed);
     }
 }
