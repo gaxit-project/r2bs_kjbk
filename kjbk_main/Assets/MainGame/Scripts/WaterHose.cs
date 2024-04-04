bf935d80213db7ab38fad4ed9f82f7efaa05e7be
jaga-Nick <shrimp1059yt@gamil.com>
2024-04-04 18:04:16 +0900

    2024/04/04 矢野 長押しで放水

--- kjbk_main/Assets/MainGame/Scripts/WaterHose.cs
+++ kjbk_main/Assets/MainGame/Scripts/WaterHose.cs
@@ -6,27 +6,49 @@ public class WaterHose : MonoBehaviour
 {
     public GameObject Child; //水のパーティクルを格納
     
-    bool WaterStatus = false;
+    bool WaterStatus = false; //放水状態
+    bool Hold = false; //長押し判定
+
     void Start(){
         Child = gameObject.transform.GetChild(0).gameObject;
     }
     void Update()
     {
-        if (PlayerRayCast.HosuStatus == true){
-            if(Input.GetMouseButton(1)){
-                if (WaterStatus == false){
-                    WaterStatus = true;
-                }else{
-                    WaterStatus = false;
-                }
+        if(Input.GetMouseButtonDown(1))
+        {
+            Hold = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Hold = false;
+        }
+        if (Hold)
+        {
+            Debug.Log("Hold");
+        }
+        if (PlayerRayCast.HosuStatus == true)
+        {
+            Debug.Log("ホースは持っている");
+            if (Hold)
+            {
+                WaterStatus = true;
             }
-        }else{
+            else
+            {
+                WaterStatus = false;
+            }
+        }
+        else
+        {
             WaterStatus = false;
         }
 
-        if (WaterStatus == true){
+        if (WaterStatus == true)
+        {
             Child.SetActive(true);
-        }else{
+        }
+        else
+        {
             Child.SetActive(false);
         }
     }
--- kjbk_main/Assets/TAKUMI/Scripts/BlazeWater.cs
+++ kjbk_main/Assets/TAKUMI/Scripts/BlazeWater.cs
@@ -7,18 +7,34 @@ public class BlazeWater : MonoBehaviour
     public GameObject Child; //水のパーティクルを格納
     
     bool WaterStatus = false;
+    bool Hold = false;
     void Start(){
         Child = gameObject.transform.GetChild(0).gameObject;
     }
     void Update()
     {
-        if (PlayerRay.HouseStatus == true){
-            if(Input.GetMouseButton(1)){
-                if (WaterStatus == false){
-                    WaterStatus = true;
-                }else{
-                    WaterStatus = false;
-                }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Hold = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Hold = false;
+        }
+        if (Hold)
+        {
+            Debug.Log("Hold");
+        }
+        if (PlayerRay.HouseStatus == true)
+        {
+            Debug.Log("ホースは持っている");
+            if(Hold)
+            {
+                   WaterStatus = true;
+            }
+            else
+            {
+                WaterStatus =false;
             }
         }else{
             WaterStatus = false;
@@ -29,6 +45,18 @@ public class BlazeWater : MonoBehaviour
         }else{
             Child.SetActive(false);
         }
+    }
+    bool MouseHold()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Hold = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            Hold = false;
+        }
+        return Hold;
     }
     
 }
