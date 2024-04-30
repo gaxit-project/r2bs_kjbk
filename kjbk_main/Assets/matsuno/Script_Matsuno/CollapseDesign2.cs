71fb7e7e881b880c5d59472ae26d2c33f6a796c8
LUCA <luklar23.oit.game@gmail.com>
2024-04-30 10:24:28 +0900

    2024/04/30 松野 HP関連の追加

---
+++ kjbk_main/Assets/matsuno/Script_Matsuno/CollapseDesign2.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollapseDesign2 : MonoBehaviour
+{
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 倒壊ゲージのデザインの定義
+    /// 
+    [SerializeField] GameObject EightDesign;
+    [SerializeField] GameObject SixDesign;
+    [SerializeField] GameObject FourDesign;
+    [SerializeField] GameObject TwoDesign;
+    [SerializeField] GameObject OneDesign;
+
+    // Start is called before the first frame update
+    void Start()
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///倒壊ゲージのデザインを隠す
+    {
+        EightDesign.SetActive(false);
+        SixDesign.SetActive(false);
+        FourDesign.SetActive(false);
+        TwoDesign.SetActive(false);
+        OneDesign.SetActive(false);
+    }
+    /// <summary>
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// 倒壊ゲージのデザインを表示する
+    public void EightHouse()
+    {
+        Debug.Log("EightHouse");
+        EightDesign.SetActive(true);          //80%の時倒壊デザインを表示
+    }
+
+    public void SixHouse()
+    {
+        Debug.Log("SixHouse");
+        EightDesign.SetActive(false);        //80%の時の倒壊デザインを非表示
+        SixDesign.SetActive(true);           //60%の時倒壊デザインを表示
+    }
+
+    public void FourHouse()
+    {
+        Debug.Log("FourHouse");
+        SixDesign.SetActive(false);          //60%の時の倒壊デザインを非表示
+        FourDesign.SetActive(true);          //40%の時倒壊デザインを表示
+    }
+
+    public void TwoHouse()
+    {
+        Debug.Log("TwoHouse");
+        FourDesign.SetActive(false);         //40%の時の倒壊デザインを非表示
+        TwoDesign.SetActive(true);           //20%の時倒壊デザインを表示
+    }
+
+    public void OneHouse()
+    {
+        Debug.Log("OneHouse");
+        TwoDesign.SetActive(false);          //20%の時の倒壊デザインを非表示
+        OneDesign.SetActive(true);           //10%の時倒壊デザインを表示
+    }
+}
---
+++ kjbk_main/Assets/matsuno/Script_Matsuno/HP_Bar.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static Unity.IO.LowLevel.Unsafe.AsyncReadManagerMetrics;
+
+public class HP_Bar : MonoBehaviour
+{
+    [SerializeField] GameObject HP1Red;
+    [SerializeField] GameObject HP2;
+    [SerializeField] GameObject HP2Red;
+    [SerializeField] GameObject HP3;
+    [SerializeField] GameObject HP3Red;
+
+    bool flag = true;
+    [SerializeField] private Renderer renderComponent3;
+    [SerializeField] private Renderer renderComponent2;
+    [SerializeField] private Renderer renderComponent1;
+
+    [SerializeField] private Renderer renderComponent4;
+    [SerializeField] private Renderer renderComponent5;
+    [SerializeField] private Renderer renderComponent6;
+
+
+    int i=3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP1Red.SetActive(false);
+        HP2.SetActive(false);
+        HP2Red.SetActive(false);
+        HP3.SetActive(false);
+        HP3Red.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+    public void HPBar()
+    {
+        if(i==3)
+        {
+            HP1Red.SetActive(true);
+            HP2Red.SetActive(true);
+            HP3Red.SetActive(true);
+            HP3.SetActive(true);
+            StartCoroutine(HPBar2());
+            Invoke(nameof(HP1RedKesu), 5f);
+            Invoke(nameof(HP2RedKesu), 5f);
+            Invoke(nameof(HP3RedKesu), 5f);
+            Invoke(nameof(HP3Kesu), 5f);
+        }
+        else if(i==2)
+        {
+            HP1Red.SetActive(true);
+            HP2Red.SetActive(true);
+            HP2.SetActive(true);
+            HP3.SetActive(true);
+            StartCoroutine(HPBar1());
+            Invoke(nameof(HP1RedKesu), 5f);
+            Invoke(nameof(HP2RedKesu), 5f);
+            Invoke(nameof(HP2Kesu), 5f);
+            Invoke(nameof(HP3Kesu), 5f);
+        }
+        i--;
+    }
+
+    IEnumerator HPBar2()
+    {
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                HP3Red.SetActive(false);
+                yield return new WaitForSeconds(0.2f);
+                HP3Red.SetActive(true);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+        HP3Red.SetActive(false);
+    }
+
+    IEnumerator HPBar1()
+    {
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                HP2Red.SetActive(false);
+                yield return new WaitForSeconds(0.2f);
+                HP2Red.SetActive(true);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+        HP2Red.SetActive(false);
+    }
+
+
+
+
+
+    public void HP3RedKesu()
+    {
+        HP3Red.SetActive(false);
+    }
+    public void HP2RedKesu()
+    {
+        HP2Red.SetActive(false);
+    }
+    public void HP1RedKesu()
+    {
+        HP1Red.SetActive(false);
+    }
+
+    public void HP3Kesu()
+    {
+        HP3.SetActive(false);
+    }
+    public void HP2Kesu()
+    {
+        HP2.SetActive(false);
+    }
+
+}
