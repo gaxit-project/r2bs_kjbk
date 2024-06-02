400b3ed574c007907e5e3ab524298af848bbb77f
LUCA <luklar23.oit.game@gmail.com>
2024-06-02 23:56:47 +0900

    2024/06/02 松野 重傷者の新設定，カウントの修正

--- kjbk_main/Assets/matsuno/Script_Matsuno/Radio_verMatsuno.cs
+++ kjbk_main/Assets/matsuno/Script_Matsuno/Radio_verMatsuno.cs
@@ -8,7 +8,7 @@ public class Radio_verMatsuno : MonoBehaviour
 {
 
     public RescuePop RPop;
-    public RescueNPC_verMatsuno RNPC; 
+    
 
     [SerializeField] GameObject EightPanel;
     [SerializeField] GameObject SixPanel;
@@ -18,10 +18,37 @@ public class Radio_verMatsuno : MonoBehaviour
     [SerializeField] GameObject RSeikou;
     [SerializeField] GameObject RShippai;
     [SerializeField] GameObject Alone;
+    //‹~•‚µ‚½‚Æ‚«‚Ì–³ü1‚ªdÒ‹~•C2`3‚ªŒyÇÒ
+    [SerializeField] GameObject FirstKitchen;
+    [SerializeField] GameObject Balcony1;
+    [SerializeField] GameObject Balcony2;
+    [SerializeField] GameObject Balcony3;
+    [SerializeField] GameObject Balcony4;
+    [SerializeField] GameObject Kitchen1;
+    [SerializeField] GameObject Kitchen2;
+    [SerializeField] GameObject Kitchen3;
+    [SerializeField] GameObject Kitchen4;
+    [SerializeField] GameObject Bath1;
+    [SerializeField] GameObject Bath2;
+    [SerializeField] GameObject Bath3;
+    [SerializeField] GameObject Bath4;
+    [SerializeField] GameObject Closet1;
+    [SerializeField] GameObject Closet2;
+    [SerializeField] GameObject Closet3;
+    [SerializeField] GameObject Closet4;
+    [SerializeField] GameObject BedRoom1;
+    [SerializeField] GameObject BedRoom2;
+    [SerializeField] GameObject BedRoom3;
+    [SerializeField] GameObject BedRoom4;
 
 
     [HideInInspector] public bool RPeople = true;
     [HideInInspector] public bool RPeople2 = true;
+
+
+    //–³ü‚ğo‚·‚Æ‚«‚Æ‚µ‚Ü‚¤‚Æ‚«‚ÌŠÔ
+    float StartTimer = 3f;   //–³ü•t‚¯‚é‚Æ‚«‚Ìƒ^ƒCƒ}[
+    float EndTimer = 5f;     //–³ü‚ğ‚«‚é‚Æ‚«‚Ìƒ^ƒCƒ}[
 
     void Start()
     {
@@ -33,6 +60,27 @@ public class Radio_verMatsuno : MonoBehaviour
         RSeikou.SetActive(false);
         RShippai.SetActive(false);
         Alone.SetActive(false);
+        FirstKitchen.SetActive(false);
+        Balcony1.SetActive(false);
+        Balcony2.SetActive(false);
+        Balcony3.SetActive(false);
+        Balcony4.SetActive(false);
+        Kitchen1.SetActive(false);
+        Kitchen2.SetActive(false);
+        Kitchen3.SetActive(false);
+        Kitchen4.SetActive(false);
+        Bath1.SetActive(false);
+        Bath2.SetActive(false);
+        Bath3.SetActive(false);
+        Bath4.SetActive(false);
+        Closet1.SetActive(false);
+        Closet2.SetActive(false);
+        Closet3.SetActive(false);
+        Closet4.SetActive(false);
+        BedRoom1.SetActive(false);
+        BedRoom2.SetActive(false);
+        BedRoom3.SetActive(false);
+        BedRoom4.SetActive(false);
     }
     /// <summary>
     /// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -41,56 +89,56 @@ public class Radio_verMatsuno : MonoBehaviour
     {
         Debug.Log("EightGauge");
         EightPanel.SetActive(true);          //80%‚Ì‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(EightGauge2), 10f);     //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(EightGauge2), EndTimer);     //5•bŒã‚ÉÁ‚·
     }
 
     public void SixGauge()
     {
         Debug.Log("SixGauge");
         SixPanel.SetActive(true);            //60%‚Ì‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(SixGauge2), 10f);       //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(SixGauge2), EndTimer);       //5•bŒã‚ÉÁ‚·
     }
 
     public void FourGauge()
     {
         Debug.Log("FourGauge");
         FourPanel.SetActive(true);           //40%‚Ì‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(FourGauge2), 10f);      //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(FourGauge2), EndTimer);      //5•bŒã‚ÉÁ‚·
     }
 
     public void TwoGauge()
     {
         Debug.Log("TwoGauge");
         TwoPanel.SetActive(true);            //20%‚Ì‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(TwoGauge2), 10f);       //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(TwoGauge2), EndTimer);       //5•bŒã‚ÉÁ‚·
     }
 
     public void OneGauge()
     {
         Debug.Log("OneGauge");
         OnePanel.SetActive(true);            //10%‚Ì‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(OneGauge2), 10f);       //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(OneGauge2), EndTimer);       //5•bŒã‚ÉÁ‚·
     }
 
     public void RSeikouRadio()
     {
         Debug.Log("Rseikou");
         RSeikou.SetActive(true);           //‹~o¬Œ÷‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(FourGauge2), 10f);      //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(FourGauge2), EndTimer);      //5•bŒã‚ÉÁ‚·
     }
 
     public void RShippaiSRadio()
     {
         Debug.Log("RShippai");
         RShippai.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(TwoGauge2), 10f);       //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(TwoGauge2), EndTimer);       //5•bŒã‚ÉÁ‚·
     }
 
     public void AloneRadio()
     {
         Debug.Log("Alone");
         Alone.SetActive(true);            //‘Sˆõ‹~‚Á‚½‚Æ‚«‚Ì–³ü‚ğ•\¦
-        Invoke(nameof(OneGauge2), 10f);       //5•bŒã‚ÉÁ‚·
+        Invoke(nameof(OneGauge2), EndTimer);       //5•bŒã‚ÉÁ‚·
     }
 
 
@@ -101,6 +149,52 @@ public class Radio_verMatsuno : MonoBehaviour
     public int RCnt(int mcnt)
     {
         return mcnt;
+    }
+
+
+    public void SymbolStop()
+    {
+        Invoke(nameof(SymbolR), StartTimer);
+    }
+    public void SymbolR()
+    {
+        int rnd = RPop.Rnd;
+        if(rnd == 0)
+        {
+            FirstKitchen.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+            Invoke(nameof(RFirstKitchen), EndTimer);
+        }
+        else if (rnd == 1)
+        {
+            Balcony1.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+            Invoke(nameof(RBalcony1), EndTimer);
+        }
+        else if (rnd == 2)
+        {
+            Kitchen1.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+            Invoke(nameof(RKitchen1), EndTimer);
+        }
+        else if (rnd == 3)
+        {
+            Bath1.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+            Invoke(nameof(RBath1), EndTimer);
+        }
+        else if (rnd == 4)
+        {
+            Closet1.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+            Invoke(nameof(RCloset1), EndTimer);
+        }
+        else if (rnd == 5)
+        {
+            BedRoom1.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+            Invoke(nameof(RBedRoom1), EndTimer);
+        }
+
+    }
+
+    public void RHintStop()
+    {
+        Invoke(nameof(RHint), StartTimer);
     }
     public void RHint()
     {
@@ -117,8 +211,17 @@ public class Radio_verMatsuno : MonoBehaviour
             {
                 if (RCnt == 0)
                 {
-                    Debug.Log("ˆêl–Ú‚ÌˆÊ’uŠm’è");
-                    //–³ü•\¦
+                    if(rnd == 0)
+                    {
+                        FirstKitchen.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                        Invoke(nameof(RFirstKitchen), EndTimer);
+                        Debug.Log("ˆêl–Ú‚ÌˆÊ’uŠm’è");
+                        //–³ü•\¦
+                    }
+                    else
+                    {
+
+                    }
                 }
                 else
                 {
@@ -126,14 +229,20 @@ public class Radio_verMatsuno : MonoBehaviour
                     {
                         if (RCnt % 3 == 1)
                         {
+                            Balcony2.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBalcony2), EndTimer);
                             Debug.Log("1-1");
                         }
                         else if (RCnt % 3 == 2)
                         {
+                            Balcony3.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBalcony3), EndTimer);
                             Debug.Log("1-2");
                         }
                         else if (RCnt % 3 == 0)
                         {
+                            Balcony4.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBalcony4), EndTimer);
                             Debug.Log("1-3");
                             RPeople2 = false;
                             //rnd1‚Ì–³ü
@@ -144,16 +253,22 @@ public class Radio_verMatsuno : MonoBehaviour
                     {
                         if (RCnt % 3 == 1)
                         {
+                            Kitchen2.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RKitchen2), EndTimer);
                             Debug.Log("2-1");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 2)
                         {
+                            Kitchen3.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RKitchen3), EndTimer);
                             Debug.Log("2-2");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 0)
                         {
+                            Kitchen4.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RKitchen4), EndTimer);
                             Debug.Log("2-3");
                             RPeople2 = false;
                             //rnd1‚Ì–³ü
@@ -164,16 +279,22 @@ public class Radio_verMatsuno : MonoBehaviour
                     {
                         if (RCnt % 3 == 1)
                         {
+                            Bath2.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBath2), EndTimer);
                             Debug.Log("3-1");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 2)
                         {
+                            Bath3.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBath3), EndTimer);
                             Debug.Log("3-2");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 0)
                         {
+                            Bath4.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBath4), EndTimer);
                             Debug.Log("3-3");
                             RPeople2 = false;
                             //rnd1‚Ì–³ü
@@ -184,16 +305,22 @@ public class Radio_verMatsuno : MonoBehaviour
                     {
                         if (RCnt % 3 == 1)
                         {
+                            Closet2.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RCloset2), EndTimer);
                             Debug.Log("4-1");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 2)
                         {
+                            Closet3.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RCloset3), EndTimer);
                             Debug.Log("4-2");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 0)
                         {
+                            Closet4.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RCloset4), EndTimer);
                             Debug.Log("4-3");
                             RPeople2 = false;
                             //rnd1‚Ì–³ü
@@ -204,16 +331,22 @@ public class Radio_verMatsuno : MonoBehaviour
                     {
                         if (RCnt % 3 == 1)
                         {
+                            BedRoom2.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBedRoom2), EndTimer);
                             Debug.Log("5-1");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 2)
                         {
+                            BedRoom3.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBedRoom3), EndTimer);
                             Debug.Log("5-2");
                             //rnd1‚Ì–³ü
                         }
                         else if (RCnt % 3 == 0)
                         {
+                            BedRoom4.SetActive(true);            //‹~o¸”s‚Ì–³ü‚ğ•\¦
+                            Invoke(nameof(RBedRoom4), EndTimer);
                             Debug.Log("5-3");
                             RPeople2 = false;
                             //rnd1‚Ì–³ü
@@ -285,6 +418,117 @@ public class Radio_verMatsuno : MonoBehaviour
     {
         Debug.Log("Alone");
         Alone.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+
+    public void RFirstKitchen()
+    {
+        Debug.Log("FirstKitchen");
+        FirstKitchen.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBalcony1()
+    {
+        Debug.Log("Balcony1");
+        Balcony1.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+
+    public void RBalcony2()
+    {
+        Debug.Log("Balcony2");
+        Balcony2.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+
+    public void RBalcony3()
+    {
+        Debug.Log("Balcony3");
+        Balcony3.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+
+    public void RBalcony4()
+    {
+        Debug.Log("Balcony4");
+        Balcony4.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+
+    public void RKitchen1()
+    {
+        Debug.Log("Kitchen1");
+        Kitchen1.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+
+    public void RKitchen2()
+    {
+        Debug.Log("Kitchen2");
+        Kitchen2.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RKitchen3()
+    {
+        Debug.Log("Kitchen3");
+        Kitchen3.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RKitchen4()
+    {
+        Debug.Log("Kitchen4");
+        Kitchen4.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBath1()
+    {
+        Debug.Log("Bath1");
+        Bath1.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBath2()
+    {
+        Debug.Log("Bath2");
+        Bath2.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBath3()
+    {
+        Debug.Log("Bath3");
+        Bath3.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBath4()
+    {
+        Debug.Log("Bath4");
+        Bath4.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RCloset1()
+    {
+        Debug.Log("Closet1");
+        Closet1.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RCloset2()
+    {
+        Debug.Log("Closet2");
+        Closet2.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RCloset3()
+    {
+        Debug.Log("Closet3");
+        Closet3.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RCloset4()
+    {
+        Debug.Log("Closet4");
+        Closet4.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBedRoom1()
+    {
+        Debug.Log("BedRoom1");
+        BedRoom1.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBedRoom2()
+    {
+        Debug.Log("BedRoom2");
+        BedRoom2.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBedRoom3()
+    {
+        Debug.Log("BedRoom3");
+        BedRoom3.SetActive(false);           //OnePanel‚ğÁ‚·
+    }
+    public void RBedRoom4()
+    {
+        Debug.Log("BedRoom4");
+        BedRoom4.SetActive(false);           //OnePanel‚ğÁ‚·
     }
 
 
--- kjbk_main/Assets/matsuno/Script_Matsuno/RescuePop.cs
+++ kjbk_main/Assets/matsuno/Script_Matsuno/RescuePop.cs
@@ -21,16 +21,33 @@ public class RescuePop : MonoBehaviour
     int a = 0;
 
     bool First = false;
+    bool RndomONOFF = true;
+
+    bool R1 = true;
+    bool R2 = true;
+    bool R3 = true;
+    bool R4 = true;
+    bool R5 = true;
+
+
+    //ランダム関連の物
+    int RStart = 1;
+    int REnd = 6;
+    int[] RandomArray = {0,0,0,0,0};
+    int cnt = 0;
 
     void Start()
     {
+        Debug.Log("こんばんわ");
+        //RBalcony.enabled = false;
         RBalcony.SetActive(false);
         RKitchen.SetActive(false);
         RBath.SetActive(false);
         RCloset.SetActive(false);
         RBedRoom.SetActive(false);
-
+        
     }
+
 
     // Update is called once per frame
     void Update()
@@ -39,11 +56,13 @@ public class RescuePop : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Z))
         {
             MCnter();
-            Radio.RHint();
+            Radio.RHintStop();
         }
         //重傷者を救ったことにする開発キー
         if (Input.GetKeyDown(KeyCode.C))
         {
+            Radio.SymbolStop();
+            RndomONOFF = true;
             Rndom();
             Rpop();
             if (First)
@@ -61,6 +80,34 @@ public class RescuePop : MonoBehaviour
             Debug.Log("重傷者を救った："+Radio.RPeople);
         }
     }
+
+    public void LightR()
+    {
+        MCnter();
+        Radio.RHintStop();
+    }
+
+    public void HeavyR()
+    {
+        Radio.SymbolStop();
+        RndomONOFF = true;
+        Rndom();
+        Rpop();
+        if (First)
+        {
+            Radio.RPeople = false;
+        }
+        First = true;
+
+        if (!Radio.RPeople2)
+        {
+            Radio.RPeople = true;
+        }
+        Radio.RPeople2 = true;
+
+        Debug.Log("重傷者を救った：" + Radio.RPeople);
+    }
+
     public int MCnter()
     {
         MCnt++;
@@ -71,32 +118,100 @@ public class RescuePop : MonoBehaviour
 
     public int Rndom() //ランダムの数を入れる関数
     {
-        Rnd = Random.Range(1, 6);   //1～5までの数をランダムに入れる
-        Debug.Log("ランダムに入れた数:" + Rnd);
+        //bool R;
+        //Rnd = 1;
+        //Rnd = Random.Range(1, 6);   //1～5までの数をランダムに入れる
+        
+
+        //for (int i = 0; i < RandomArray.Length; i++)
+        //{
+        //    if (RandomArray[i] == Rnd)
+        //    {
+        //        Rnd = Random.Range(1, 6);
+        //        Debug.Log("nnnnnnnnnnnnnnnn");
+        //        i = 0;
+        //    }
+        //}
+        //RandomArray[cnt] = Rnd;
+        //Debug.Log("ランダムに入れた数:" + Rnd);
+        //cnt++;
+
+        //for (int i = 0; i < RandomArray.Length; i++)
+        //{
+        //    Debug.Log(RandomArray[i]);
+        //}
+        while(RndomONOFF)
+        {
+            if (!R1 && !R2 && !R3 && !R4 && !R5)
+            {
+                
+                break;
+            }
+            else
+            {
+                Rnd = Random.Range(1, 6);
+                Debug.Log(Rnd);
+
+
+                if (Rnd == 1 && R1 || Rnd == 2 && R2 || Rnd == 3 && R3 || Rnd == 4 && R4 || Rnd == 5 && R5)
+                {
+
+                    if (Rnd == 1)
+                    {
+                        R1 = false;
+                    }
+                    else if (Rnd == 2)
+                    {
+                        R2 = false;
+                    }
+                    else if (Rnd == 3)
+                    {
+                        R3 = false;
+                    }
+                    else if (Rnd == 4)
+                    {
+                        R4 = false;
+                    }
+                    else if (Rnd == 5)
+                    {
+                        R5 = false;
+                    }
+                    RndomONOFF = false;
+                    break;
+                }
+            }
+           
+            
+        }
+
+
+
+
         return Rnd;
     }
     //重傷者を救ったらこれを起動する
     public void Rpop()
     {
         int rndom = Pop.Rnd;
+        Debug.Log("ポップする際の受け取り:" + rndom);
 
-        if(Rnd == 1)
+        if(rndom == 1)
         {
             RBalcony.SetActive(true);
         }
-        else if (Rnd == 2)
+        else if (rndom == 2)
         {
             RKitchen.SetActive(true);
         }
-        else if (Rnd == 3)
+        else if (rndom == 3)
         {
             RBath.SetActive(true);
         }
-        else if (Rnd == 4)
+        else if (rndom == 4)
         {
             RCloset.SetActive(true);
         }
-        else if (Rnd == 5)
+        else if (rndom == 5)
         {
             RBedRoom.SetActive(true);
         }
