eccef3f4c6586158d1bf25f4899f2098942e52c1
jaga-Nick <shrimp1059yt@gmail.com>
2024-06-25 23:15:01 +0900

    2024/06/25 矢野: 追加

--- kjbk_main/Assets/matsuno/Script_Matsuno/Radio_ver3.cs
+++ kjbk_main/Assets/matsuno/Script_Matsuno/Radio_ver3.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
