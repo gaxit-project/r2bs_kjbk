b887f0fe35741a6b8eaae7869b0343e499299fc0
jaga-Nick <shrimp1059yt@gmail.com>
2024-07-04 14:40:36 +0900

    2024/07/04矢野：ビルドバグ解消

--- kjbk_main/Assets/MainGame/Scripts/main/Fire/FireLv.cs
+++ kjbk_main/Assets/MainGame/Scripts/main/Fire/FireLv.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
+//using static UnityEditor.PlayerSettings;
 
 public class FireLv : MonoBehaviour
 {
