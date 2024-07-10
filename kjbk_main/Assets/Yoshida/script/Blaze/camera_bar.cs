886a440ebae7c99cfb44f3dade97efdbbe4ef7a0
吉田 翔湧 <towaoyaji200364@gmail.com>
2024-07-10 19:31:57 +0900

    2024/07/10 吉田 : 画面縁の表示を修正

--- kjbk_main/Assets/Yoshida/script/Blaze/camera_bar.cs
+++ kjbk_main/Assets/Yoshida/script/Blaze/camera_bar.cs
@@ -12,11 +12,19 @@ public class camera_bar : MonoBehaviour
     public GameObject UnderPlane;
     public GameObject LeftPlane;
     public GameObject RightPlane;
+    public GameObject UpRightPlane;
+    public GameObject UpLeftPlane;
+    public GameObject UnderRightPlane;
+    public GameObject UnderLeftPlane;
 
     private MeshRenderer UpMesh;
     private MeshRenderer UnderMesh;
     private MeshRenderer LeftMesh;
     private MeshRenderer RightMesh;
+    private MeshRenderer UpRightMesh;
+    private MeshRenderer UpLeftMesh;
+    private MeshRenderer UnderRightMesh;
+    private MeshRenderer UnderLeftMesh;
 
     // Start is called before the first frame update
     void Start()
@@ -27,17 +35,25 @@ public class camera_bar : MonoBehaviour
         UnderMesh = UnderPlane.GetComponent<MeshRenderer>();
         LeftMesh = LeftPlane.GetComponent<MeshRenderer>();
         RightMesh = RightPlane.GetComponent<MeshRenderer>();
+        UpRightMesh = UpRightPlane.GetComponent<MeshRenderer>();
+        UpLeftMesh = UpLeftPlane.GetComponent<MeshRenderer>();
+        UnderRightMesh = UnderRightPlane.GetComponent<MeshRenderer>();
+        UnderLeftMesh = UnderLeftPlane.GetComponent<MeshRenderer>();
 
-        UpMesh.material.color = UpMesh.material.color - new Color32(255, 0, 0, 255);
+        UpMesh.material.color = new Color32(255, 0, 0, 255);
         UnderMesh.material.color = new Color32(255, 0, 0, 255);
         LeftMesh.material.color = new Color32(255, 0, 0, 255);
         RightMesh.material.color = new Color32(255, 0, 0, 255);
+        UpRightMesh.material.color = new Color32(255, 0, 0, 255);
+        UpLeftMesh.material.color = new Color32(255, 0, 0, 255);
+        UnderRightMesh.material.color = new Color32(255, 0, 0, 255);
+        UnderLeftMesh.material.color = new Color32(255, 0, 0, 255);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (ray.Up || ray.UpRight || ray.UpLeft)
+        if (ray.Up)
         {
             UpMesh.material.color = new Color32(255, 0, 0, 255);
         }
@@ -46,7 +62,7 @@ public class camera_bar : MonoBehaviour
             UpMesh.material.color = new Color32(255, 0, 0, 0);
         }
 
-        if (ray.Under || ray.UnderLeft ||ray.UnderRight)
+        if (ray.Under)
         {
             UnderMesh.material.color = new Color32(255, 0, 0, 255);
         }
@@ -55,7 +71,7 @@ public class camera_bar : MonoBehaviour
             UnderMesh.material.color = new Color32(255, 0, 0, 0);
         }
 
-        if (ray.Right || ray.UpRight || ray.UnderRight)
+        if (ray.Right)
         {
             RightMesh.material.color = new Color32(255, 0, 0, 255);
         }
@@ -64,13 +80,49 @@ public class camera_bar : MonoBehaviour
             RightMesh.material.color = new Color32(255, 0, 0, 0);
         }
 
-        if (ray.Left || ray.UpLeft || ray.UnderLeft)
+        if (ray.Left)
         {
             LeftMesh.material.color = new Color32(255, 0, 0, 255);
         }
         else
         {
             LeftMesh.material.color = new Color32(255, 0, 0, 0);
+        }
+
+        if (ray.UpRight)
+        {
+            UpRightMesh.material.color = new Color32(255, 0, 0, 255);
+        }
+        else
+        {
+            UpRightMesh.material.color = new Color32(255, 0, 0, 0);
+        }
+
+        if (ray.UnderRight)
+        {
+            UnderRightMesh.material.color = new Color32(255, 0, 0, 255);
+        }
+        else
+        {
+            UnderRightMesh.material.color = new Color32(255, 0, 0, 0);
+        }
+
+        if (ray.UpLeft)
+        {
+            UpLeftMesh.material.color = new Color32(255, 0, 0, 255);
+        }
+        else
+        {
+            UpLeftMesh.material.color = new Color32(255, 0, 0, 0);
+        }
+
+        if (ray.UnderLeft)
+        {
+            UnderLeftMesh.material.color = new Color32(255, 0, 0, 255);
+        }
+        else
+        {
+            UnderLeftMesh.material.color = new Color32(255, 0, 0, 0);
         }
     }
 }
