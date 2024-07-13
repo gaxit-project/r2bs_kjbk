9cfc35e482c5f70d82c43b56999bce5df23acffc
tai <taiyaki1026@icloud.com>
2024-07-14 08:05:00 +0900

    2024/07/14 太陽 : リザルト点数表示

--- kjbk_main/Assets/MainGame/Scripts/result/ResultScore.cs
+++ kjbk_main/Assets/MainGame/Scripts/result/ResultScore.cs
@@ -9,17 +9,27 @@ public class ResultScore : MonoBehaviour
     float countscore = 0;
     float displaytime = 0;
     float injureddisplay = 1.5f;
-    float timedisplay = 2.0f;
-    float hpdisplay = 2.5f;
-    float totaldisplay = 3.5f;
+    float injureddisplay2 = 2f;
+    float timedisplay = 2.5f;
+    float timedisplay2 = 3f;
+    float hpdisplay = 3.5f;
+    float hpdisplay2 = 4f;
+    float faileddisplay = 4.5f;
+    float faileddisplay2 = 5f;
+    float totaldisplay = 6f;
     float display = 0.1f;
     public GameObject ResultCanvas;
     private int score;
     private int hantei = SampleManager.hantei;
     private Text textScore;
     public  Text textPeople;
+    public Text textPeople2;
     public Text textTime;
+    public Text textTime2;
     public Text textHP;
+    public Text textHP2;
+    public Text textFailure;
+    public Text textFailure2;
     public Text textTotal;
     private GameObject Clear;
     private GameObject Failed;
@@ -35,13 +45,18 @@ public class ResultScore : MonoBehaviour
     int hp;
     int timepoint;
     int hppoint;
+    int people2;
+    int timepoint2;
+    int hppoint2;
     int failedpeople;
     int failedtime;
     int failedhp;
     bool clearflag;
     int failedtotal;
+    int pt;
 
     int RescueCnt;
+    int random;
     void Start()
     {
         Time.timeScale = 1.0f;
@@ -51,6 +66,11 @@ public class ResultScore : MonoBehaviour
         //textTime = GameObject.Find("Time").GetComponent<Text>();
         //textHP = GameObject.Find("HP").GetComponent<Text>();
         //textTotal = GameObject.Find("Total").GetComponent<Text>();
+        textPeople2 = GameObject.Find("People2").GetComponent<Text>();
+        textTime2 = GameObject.Find("Time2").GetComponent<Text>();
+        textHP2 = GameObject.Find("HP2").GetComponent<Text>();
+        textFailure = GameObject.Find("Failure").GetComponent<Text>();
+        textFailure2 = GameObject.Find("Failure2").GetComponent<Text>();
         Clear = GameObject.Find("Clear");
         Failed = GameObject.Find("Failed");
         FireRoof = GameObject.Find("FireRoof");
@@ -62,46 +82,98 @@ public class ResultScore : MonoBehaviour
         ScoreCount();
         score2 = RescueCnt * 100;
         people = score + score2;
-        timepoint = (int)a * 5;
+        people2 = people;
+        
+        timepoint = (int)a * 3;
+        timepoint2 = timepoint;
         hppoint = hp * 100;
+        hppoint2 = hppoint;
         
         Result(PlayerPrefs.GetString("Result"));
     }
     private void Update()
     {
-        
+        total = (int)(people + timepoint + hppoint);
         countscore += Time.deltaTime;
+        displaytime += Time.deltaTime;
         if(countscore >= injureddisplay)
         {
                 textPeople.text = "People:" + people.ToString();
+            textPeople2.text = "People\n" + people2.ToString();
+            
+        }
+        if (countscore >= injureddisplay2)
+        {
+            textTotal.text = "0+" + people.ToString();
         }
         if (countscore >= timedisplay)
         {
                 textTime.text = "Time:" + timepoint.ToString();
+            textPeople2.text = "";
+            textTime2.text = "Time\n" + timepoint2.ToString();
+            
+
+        }
+        if (countscore >= timedisplay2)
+        {
+            textTotal.text = people.ToString() + "+" + timepoint.ToString();
         }
         if (countscore >= hpdisplay)
         {
                 textHP.text = "HP:" + hppoint.ToString();
+            textTime2.text = "";
+            textHP2.text = "HP\n" + hppoint2.ToString();
+            
         }
-        if (countscore >= totaldisplay)
+        if (countscore >= hpdisplay2)
         {
+            pt = (int)(people + timepoint);
+            textTotal.text = pt.ToString() + "+" + hppoint.ToString();
+        }
+        if (countscore >= faileddisplay)
+        {
+            textHP2.text = "";
             if (clearflag == false)
             {
-                failedtotal = (int)(people + timepoint + hppoint) - 1000;
-                total = (int)(people + timepoint + hppoint);
+                textFailure.text = "Failed:-500";
+                textFailure2.text = "Failed\n-500";
+                
+            }
+            else
+            {
+                textFailure.text = "Failed:0";
+            }
+            
+        }
+        if (countscore >= faileddisplay2)
+        {
+            
+            if (clearflag == false)
+            {
+                textTotal.text = total.ToString() + "-500";
+            }
+        }
+
+        if (countscore >= totaldisplay)
+        {
+            textFailure2.text = "";
+            if (clearflag == false)
+            {
+                failedtotal = (int)(people + timepoint + hppoint) - 500;
+                
                 if (failedtotal > 0)
                 {
-                    textTotal.text = "Total:" + total.ToString() + "-1000=" + failedtotal.ToString();
+                    textTotal.text = failedtotal.ToString();
                 }
                 else
                 {
-                    textTotal.text = "Total:" + total.ToString() + "-1000=" + "0";
+                    textTotal.text = total.ToString();
                 }
             }
             else if(clearflag == true)
             {
                 total = (int)(people + timepoint + hppoint);
-                textTotal.text = "Total:" + total.ToString();
+                textTotal.text = total.ToString();
             }
             
         }
