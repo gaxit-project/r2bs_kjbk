6a69b7e2ec825f79640d0c1e546399869a81474d
jaga-Nick <shrimp1059yt@gamil.com>
2024-09-06 23:36:15 +0900

    2024/09/06矢野；ItemTake修正

--- kjbk_main/Assets/TAKUMI/Scripts/Player/ItemTake.cs
+++ kjbk_main/Assets/TAKUMI/Scripts/Player/ItemTake.cs
@@ -8,6 +8,9 @@ public class ItemTake : MonoBehaviour
     bool[] ItemFlag = new bool[9];
     bool[] GetItem = new bool[9];
     public static int ItemCount = 3;
+
+    int[] Item = new int[3];
+
     bool ItemGetFlag = false;
 
     public SubMissionItem SMI;
@@ -54,72 +57,63 @@ public class ItemTake : MonoBehaviour
             {
                 //
                 SMI.ItemActive(0);
-                MMUI.MissionUpgread("■マグカップを持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■マグカップを探す", ItemID(0), 0);
                 FlagReset();
             }
             else if (ItemFlag[1] && Take && GetItem[1])
             {
                 //
                 SMI.ItemActive(1);
-                MMUI.MissionUpgread("■スマホを持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■スマホを探す", ItemID(1), 0);
                 FlagReset();
             }
             else if (ItemFlag[2] && Take && GetItem[2])
             {
                 //
                 SMI.ItemActive(2);
-                MMUI.MissionUpgread("■シャンプーを持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■シャンプーを探す", ItemID(2), 0);
                 FlagReset();
             }
             else if (ItemFlag[3] && Take && GetItem[3])
             {
                 //
                 SMI.ItemActive(3);
-                MMUI.MissionUpgread("■Tシャツを持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■Tシャツを探す", ItemID(3), 0);
                 FlagReset();
             }
             else if (ItemFlag[4] && Take && GetItem[4])
             {
                 //
                 SMI.ItemActive(4);
-                MMUI.MissionUpgread("■絵画を持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■絵画を探す", ItemID(4), 0);
                 FlagReset();
             }
             else if (ItemFlag[5] && Take && GetItem[5])
             {
                 //
                 SMI.ItemActive(5);
-                MMUI.MissionUpgread("■ゲーム機を持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■ゲーム機を探す", ItemID(5), 0);
                 FlagReset();
             }
             else if (ItemFlag[6] && Take && GetItem[6])
             {
                 //
                 SMI.ItemActive(6);
-                MMUI.MissionUpgread("■写真を持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■写真を探す", ItemID(6), 0);
                 FlagReset();
             }
             else if (ItemFlag[7] && Take && GetItem[7])
             {
                 //
                 SMI.ItemActive(7);
-                MMUI.MissionUpgread("■くまのぬいぐるみを持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■くまのぬいぐるみを探す", ItemID(7), 0);
                 FlagReset();
             }
             else if (ItemFlag[8] && Take && GetItem[8])
             {
                 //
                 SMI.ItemActive(8);
-                MMUI.MissionUpgread("■花を持って帰る", ItemCount, 0);
-                ItemCount--;
+                MMUI.MissionUpgread("■花を探す", ItemID(8), 0);
                 FlagReset();
             }
 
@@ -142,7 +136,25 @@ public class ItemTake : MonoBehaviour
             SMI.ShaderOn(num);
             SMI.ItemButtonActive(num);
             Debug.Log("アイテムセット　" + num);
+            Item[ItemCount - 1] = num;
+            ItemCount--;
         }
+    }
+
+    public int ItemID(int num)
+    {
+        for(int i = 0; i < Item.Length; i++)
+        {
+            if (Item[i] == num)
+            {
+                return i + 1;
+            }
+            else
+            {
+                return -1;//エラー
+            }
+        }
+        return -1;//エラー
     }
 
     public void ItemFlagSet(bool Flag, int num)
