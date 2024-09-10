c6149528283f0d1e1f4391fecb18ba17a46be7e0
jaga-Nick <shrimp1059yt@gamil.com>
2024-09-11 02:52:45 +0900

    2024/09/11矢野；重傷者担ぎ修正

--- kjbk_main/Assets/TAKUMI/Scripts/Input/HoldGauge.cs
+++ kjbk_main/Assets/TAKUMI/Scripts/Input/HoldGauge.cs
@@ -13,6 +13,9 @@ internal class HoldGauge : MonoBehaviour
     float progress;
     bool gaugeActivated = false;
 
+    float lockTime = 0;
+
+
     private void Awake()
     {
         if (_hold == null) return;
@@ -26,36 +29,53 @@ internal class HoldGauge : MonoBehaviour
         progress = 0.0f;
         gaugeStatus = false;
         gaugeActivated = false;
+        // 進捗をゲージに反映
+        _gaugeImage.fillAmount = progress;
+
+        lockTime = 0;
+
+        _holdAction.Disable();  // Actionを一旦無効化
     }
 
     private void Update()
     {
         if (_holdAction == null) return;
 
-        // 長押しの進捗を取得
-        progress = _holdAction.GetTimeoutCompletionPercentage();
-
-        // 進捗をゲージに反映
-        _gaugeImage.fillAmount = progress;
-
-        // 進捗が1以上になったときの処理
-        if (progress >= 1 && !gaugeActivated)
+        if(lockTime <= 0.5f)
         {
-            // gaugeStatusをtrueにする
-            gaugeStatus = true;
-            gaugeActivated = true;
-
-            // ゲージをリセット
-            _gaugeImage.fillAmount = 0;
-            _holdAction.Disable();  // Actionを一旦無効化
+            lockTime += Time.deltaTime;
+        }
+        else
+        {
             _holdAction.Enable();   // すぐに有効化して次の入力に備える
-            progress = 0.0f;
+
+            // 長押しの進捗を取得
+            progress = _holdAction.GetTimeoutCompletionPercentage();
+
+            // 進捗をゲージに反映
+            _gaugeImage.fillAmount = progress;
+
+            // 進捗が1以上になったときの処理
+            if (progress >= 1 && !gaugeActivated)
+            {
+                // gaugeStatusをtrueにする
+                gaugeStatus = true;
+                gaugeActivated = true;
+
+                // ゲージをリセット
+                _gaugeImage.fillAmount = 0;
+                _holdAction.Disable();  // Actionを一旦無効化
+                _holdAction.Enable();   // すぐに有効化して次の入力に備える
+                progress = 0.0f;
+            }
+            else if (gaugeActivated)
+            {
+                // 1フレーム後にgaugeStatusをfalseに戻す
+                gaugeStatus = false;
+                gaugeActivated = false;
+                lockTime = 0f;
+            }
         }
-        else if (gaugeActivated)
-        {
-            // 1フレーム後にgaugeStatusをfalseに戻す
-            gaugeStatus = false;
-            gaugeActivated = false;
-        }
+
     }
 }
